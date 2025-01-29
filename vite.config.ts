50cb2e2dd0bec62b5b104ca30d900b2ca9886509
khuzama <talhanew0004@gmail.com>
2025-01-29 19:09:36 +0500

    basiccommit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
+import sassDts from 'vite-plugin-sass-dts';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [ sassDts()], // Remove `vue()` if not using Vue
+
+});
